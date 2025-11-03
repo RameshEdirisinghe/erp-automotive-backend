b649237cdfc0fdf7fe5dc280a521829141e4a8ed
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-11-03 09:39:33 +0530

    Database Connection Issue Resolved

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,13 +4,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://mainresidue_db_user:qwerty2025@erpjapan.8u5fxzc.mongodb.net/?appName=ERPJapan',
-    ),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_URI');
+        return { uri };
+      },
     }),
   ],
+  exports: [MongooseModule],
 })
 export class DatabaseModule {}
