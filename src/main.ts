dbabc408c5c279447ca0edd3408bb712ca9f76e6
RameshEdirisinghe <e2145434@gmail.com>
2025-11-03 11:31:45 +0530

    remove rocket emoji from server startup log message

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
   console.log(
-    `🚀 Server running on http://localhost:${process.env.PORT ?? 3000}`,
+    ` Server running on http://localhost:${process.env.PORT ?? 3000}`,
   );
 }
 bootstrap();
